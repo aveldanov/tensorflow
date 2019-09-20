8c20148c77bb778a94bb7b0aa569a3b88217cfde
Anton Veldanov <anton.veldanov@gmail.com>
2019-09-19 20:27:28 -0700

    tensor

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
 const xs = tf.randomNormal([100, 10]);
 const ys = tf.randomNormal([100, 1]);
 
-
 model.fit(xs, ys, {
   epochs: 100,
   callbacks: {
