b5702b43e54962016cc30bda345c75b5c505f029
Anton Veldanov <anton.veldanov@gmail.com>
2019-11-09 14:32:26 -0800

    Tensor

--- TensorLinkedIn/index.js
+++ TensorLinkedIn/index.js
@@ -1,29 +1,11 @@
+// define model
 
-const shape = [4, 2];
-// const data = tf.tensor([[4, 6], [5, 9], [13, 25]]);
-const data = tf.tensor([4, 6, 5, 9, 13, 25, 1, 5], shape);
-
-
-// set variables
-
-const data2 = tf.variable(tf.zeros([8]));
+const simpleAdd = (input1, input2) => {
+  //tidy used to free up GPU memory
 
 
-
-//print data
-// data2.print();
+  return tf.tidy(() => {
 
 
-data2.assign(tf.tensor1d([4, 12, 5, 6, 56, 3, 45, 3]));
-
-// data2.print();
-
-
-const data3 = tf.tensor1d([4, 5, 6, 9]);
-const data4 = tf.tensor1d([5, 4, 23, 45])
-
-
-data3.print();
-data4.print();
-
-data3.add(data4).print();
+  });
+}
